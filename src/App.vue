69f6bf6fb304b02e923264ad4a475e15467fd61e
Kim Le <lekim@live.dk>
2023-05-15 09:15:01 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,59 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <div
+    class="max-w-screen-sm mx-8 sm:mx-auto mt-8 border rounded-lg overflow-hidden"
+  >
+    <div class="bg-green-500 px-6 py-4 text-white flex justify-between">
+      <div>
+        <router-link
+          :class="`py-1 px-3 rounded-md text-white font-bold select-none ${
+            route.name === R.TODO ? 'bg-green-600' : ''
+          }`"
+          :to="{ name: R.TODO }"
+        >
+          Todo
+        </router-link>
+        <router-link
+          :class="`py-1 px-3 rounded-md text-white font-bold select-none ml-4 ${
+            route.name === R.DONE ? 'bg-green-600' : ''
+          }`"
+          :to="{ name: R.DONE }"
+        >
+          Done
+        </router-link>
+      </div>
+      <div class="flex">
+        <div v-if="todoStore.todoList.length" class="flex items-center mr-4">
+          <svg class="w-5 inline-block fill-current" viewBox="0 0 24 24">
+            <path :d="mdiCheckCircleOutline" />
+          </svg>
+          <span class="ml-1">{{ todoStore.todoList.length }}</span>
+        </div>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+        <div v-if="todoStore.doneList.length" class="flex items-center">
+          <svg class="w-5 inline-block fill-current" viewBox="0 0 24 24">
+            <path :d="mdiCheckCircle" />
+          </svg>
+          <span class="ml-1">{{ todoStore.doneList.length }}</span>
+        </div>
+      </div>
     </div>
-  </header>
-
-  <RouterView />
+    <div class="px-6 py-4 transition-all duration-300">
+      <router-view />
+    </div>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { useRoute } from 'vue-router'
+import { mdiCheckCircle, mdiCheckCircleOutline } from '@mdi/js'
+import { R } from './router'
+import { todoStore } from './store/todoStore'
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+export default defineComponent({
+  setup () {
+    const route = useRoute()
+    return { R, route, todoStore, mdiCheckCircle, mdiCheckCircleOutline }
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+})
+</script>
